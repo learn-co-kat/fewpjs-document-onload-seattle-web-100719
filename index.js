7329b980704e48b60eb01b9f4185e3cc144a2bda
learn-co-kat <twothistles@gmail.com>
2019-11-18 21:24:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+     const loadedComment = document.querySelector("#text");
+     loadedComment.innerHTML = "This is really cool!"
+})
